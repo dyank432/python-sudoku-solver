bba187cb0fa3bd652aa3cadf248706383a2e6f77
dyank432 <dyank432@mtroyal.ca>
2022-05-24 23:29:07 -0600

    v1.0 update
    -findEmpty, validate and solve functions added.
    -also added code to print the board before and after the program runs.
    
    current version works, but may come back to this project

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -25,5 +25,69 @@ def printBoard(board):
             else:
                 print(str(board[i][j]) + " ", end="")
 
-printBoard(board)
+#returns the row and column containing value of 0 in the board (2d array)
+def findEmpty(board):
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 0:
+                return (i, j)
+    
+    #when there are no more empty squares, return nothing
+    return None
 
+def validate(board, n, pos):
+
+    #check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == n and pos[1] != i:
+            return False
+
+    #check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == n and pos[0] != i:
+            return False
+
+    #check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == n and (i,j) != pos:
+                return False
+
+    return True
+
+#uses recursion, when the board is full it is solved.
+#utilizes the backtrack algorithm
+def solve(board):
+
+    find = findEmpty(board)
+
+    #board not full (base case)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if validate(board, i, (row,col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+            
+            board[row][col] = 0
+    
+    return False
+
+print("~~~~~~~~~~~~~~~~~~~~~~~")
+print("BEFORE:")
+print("~~~~~~~~~~~~~~~~~~~~~~~")
+printBoard(board)
+solve(board)
+print("~~~~~~~~~~~~~~~~~~~~~~~")
+print("SOLUTION")
+print("~~~~~~~~~~~~~~~~~~~~~~~")
+
+printBoard(board)
